f6467afa893c4232c6d546c81cae2f62b0b952f4
Austromo <moritz.gruber.linz@gmail.com>
2024-12-14 02:22:04 +0100

    feat: split keycloak resources in seperated files

--- keycloak-demo/src/main/java/at/htl/keycloakDemo/resources/RealmResource.java
+++ keycloak-demo/src/main/java/at/htl/keycloakDemo/resources/RealmResource.java
@@ -2,42 +2,23 @@ package at.htl.keycloakDemo.resources;
 
 import io.quarkus.logging.Log;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
 
-@Path("/admin")
-public class RolesResource {
+@Path("/admin/realms")
+public class RealmResource {
     @Inject
     Keycloak keycloak;
 
-    @GET
-    @Path("/roles")
-    public Response getRoles() {
-        //Log.error(keycloak.realm("quarkus").users().count());
-        //Log.error(keycloak.realm("quarkus").roles().list());
-
-        //keycloak.realm("my-realm").clients().get("quarkus-client").authorization().getSettings();
-
-        return Response.ok(
-            keycloak
-                .realm("my-realm")
-                .roles()
-                .list()
-        ).build();
-    }
-
     @POST
-    @Path("/realm/{realmName}")
+    @Path("/{realmName}")
     public Response createRealm(@PathParam("realmName") String realmName) {
-        /*var realm = keycloak.realms()
-                .findAll()
-                .stream()
-                .filter(r -> Objects.equals(r.getId(), realmName))
-                .findFirst()
-                .orElse(null);*/
         if (realmName == null) {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
@@ -56,8 +37,15 @@ public class RolesResource {
         return Response.ok().build();
     }
 
+    @DELETE
+    @Path("/{realmName}")
+    public Response deleteRealm(@PathParam("realmName") String realmName) {
+        keycloak.realms().realm(realmName).remove();
+        return Response.ok().build();
+    }
+
     @POST
-    @Path("/realm/{realmName}/{clientName}")
+    @Path("/{realmName}/client/{clientName}")
     public Response createClientInRealm(@PathParam("realmName") String realmName, @PathParam("clientName") String clientName) {
         if (realmName == null || clientName == null) {
             return Response.status(Response.Status.BAD_REQUEST).build();
@@ -78,14 +66,7 @@ public class RolesResource {
     }
 
     @DELETE
-    @Path("/realm/{realmName}")
-    public Response deleteRealm(@PathParam("realmName") String realmName) {
-        keycloak.realms().realm(realmName).remove();
-        return Response.ok().build();
-    }
-
-    @DELETE
-    @Path("/realm/{realmName}/{clientName}")
+    @Path("/{realmName}/client/{clientName}")
     public Response deleteClientInRealm(@PathParam("realmName") String realmName, @PathParam("clientName") String clientName) {
         if (realmName == null || clientName == null) {
             return Response.status(Response.Status.BAD_REQUEST).build();
