08d8f28da32dc2815b1b0f6ad097251cf8630d21
veqox <veqoxao@gmail.com>
2024-12-19 22:32:54 +0100

    chore: add traefik self signed https fix: nginx & traefik ingress controller chore: add acme challenge diagram

--- reverse-proxy/traefik-tls-self-signed/scripts/generate-self-signed-certifcate.sh
+++ reverse-proxy/traefik-tls-self-signed/scripts/generate-self-signed-certifcate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# run this from the nginx-tls-self-signed directory
+# run this from the traefik-tls-self-signed directory
 
 DOMAIN="echo.localhost"
 
@@ -7,9 +7,7 @@ CERTS_DIR="${PWD}/certs"
 KEY_FILE="${CERTS_DIR}/private.key"
 SIGN_REQUEST_FILE="${CERTS_DIR}/request.csr"
 CERT_FILE="${CERTS_DIR}/certificate.crt"
-DHPARAM_FILE="${CERTS_DIR}/dhparam.pem"
 
 openssl genrsa -out $KEY_FILE 4096
 openssl req -new -key $KEY_FILE -out $SIGN_REQUEST_FILE -subj "/CN=${DOMAIN}"
 openssl x509 -req -days 365 -in $SIGN_REQUEST_FILE -signkey $KEY_FILE -out $CERT_FILE
-openssl dhparam -out $DHPARAM_FILE 4096
